38935cdee835105832840058a8a1d1c9f2ed72b0
David Guerrero Gómez <95078399+DaGuerGom@users.noreply.github.com>
2024-09-09 16:53:39 +0200

    first commit

---
+++ src/api/TeamAPI.ts
@@ -0,0 +1,50 @@
+import api from "@/lib/axios"
+import { Project, TeamMember, TeamMemberForm, teamMembersSchema } from "../types"
+import { isAxiosError } from "axios"
+
+export async function findUserByEmail({projectId,formData}:{projectId:Project['_id'],formData:TeamMemberForm}){
+    try {
+        const {data}=await api.post(`/projects/${projectId}/team/find`,formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function addMember({projectId,id}:{projectId:Project['_id'],id:TeamMember['_id']}){
+    try {
+        const {data}=await api.post<string>(`/projects/${projectId}/team`,{id})
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectTeam(projectId:Project['_id']){
+    try {
+        const {data}=await api(`/projects/${projectId}/team`)
+        const response=teamMembersSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function removeUserFromProject({projectId,id}:{projectId:Project['_id'],id:TeamMember['_id']}){
+    try {
+        const {data}=await api.delete<string>(`/projects/${projectId}/team/${id}`)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
---
+++ src/components/team/SearchResult.tsx
@@ -0,0 +1,53 @@
+import { addMember } from '@/api/TeamAPI'
+import { TeamMember } from '@/types/index'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+type SearchResultProps = {
+    user: TeamMember
+    reset: () => void
+}
+
+export default function SearchResult({ user, reset }: SearchResultProps) {
+
+    const params = useParams()
+    const projectId = params.projectId!
+    const queryClient=useQueryClient()
+    const navigate=useNavigate()
+
+    const {mutate}=useMutation({
+        mutationFn:addMember,
+        onError:(error)=>{
+            console.log(error)
+            toast.error(error.message)
+        },
+        onSuccess:(data)=>{
+            toast.success(data)
+            reset()
+            navigate(location.pathname, {replace: true})
+            queryClient.invalidateQueries({queryKey: ['projectTeam', projectId]})
+        }
+      })
+
+    const handleAddUserToProject = () => {
+        const data = {
+            projectId,
+            id:user._id
+        }
+        mutate(data)
+    }
+
+    return (
+        <div>
+        <p className="mt-10 text-center font-bold">Resultado:</p>
+            <div className="flex justify-between items-center">
+                <p>{user.name}</p>
+                <button
+                    className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer"
+                    onClick={handleAddUserToProject}
+                >Agregar al Proyecto</button>
+            </div>
+        </div>
+    )
+}
